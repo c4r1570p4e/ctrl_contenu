bc90b2d4c4435ceacc771c4487347a38f2babc0d
cl <Christophe@PortableHPCL>
2013-09-10 22:20:38 +0200

    ajout ReadWriteLock

--- ctrl-contenu-war/src/main/java/org/cl/contenu/rest/LatenceBean.java
+++ ctrl-contenu-war/src/main/java/org/cl/contenu/rest/LatenceBean.java
@@ -1,30 +1,58 @@
 package org.cl.contenu.rest;
 
 import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-import lombok.Data;
+import lombok.Getter;
 
 import org.springframework.stereotype.Component;
 
 import com.google.common.base.Throwables;
 
 @Component
-@Data
 public class LatenceBean {
 
-	private int min = 0;
-	private int max = 0;
+	private ReadWriteLock rrwl = new ReentrantReadWriteLock(true);
+	private Lock readLock = rrwl.readLock();
+	private Lock writeLock = rrwl.writeLock();
+
+	@Getter
+	private volatile int min = 0;
+	@Getter
+	private volatile int max = 0;
 
 	public void changeLatence(int min, int max) {
-		this.min = min;
-		this.max = max;
+
+		writeLock.lock();
+
+		try {
+			this.min = min;
+			this.max = max;
+		} finally {
+			writeLock.unlock();
+		}
+
 	}
 
 	public void doLatence() {
 
-		if (min != 0 && max != 0) {
+		int lmin = 0;
+		int lmax = 0;
+
+		readLock.lock();
+
+		try {
+			lmin = this.min;
+			lmax = this.max;
+		} finally {
+			readLock.unlock();
+		}
+
+		if (lmin != 0 && lmax != 0) {
 			Random random = new Random();
-			double attente = random.nextDouble() * (double) (max - min) + (double) min;
+			double attente = random.nextDouble() * (double) (lmax - lmin) + (double) lmin;
 
 			try {
 				Thread.sleep((long) (attente * 1000));
